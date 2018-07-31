0e2ae7ab38458acd70061e9df106daca1395dfe6
aljun <aljunma20@gmail.com>
2018-07-31 13:55:14 +0800

    add featured image
    add featured image

--- functions.php
+++ functions.php
@@ -9,6 +9,15 @@
 
 
 if ( ! function_exists( 'thegoodartisan_setup' ) ) :
+
+
+    /*
+     * Enable support for Post Thumbnails on posts and pages.
+     *
+     * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+     */
+    add_theme_support( 'post-thumbnails' );
+
 	/**
 	 * Sets up theme defaults and registers support for various WordPress features.
 	 *
